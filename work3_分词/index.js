5fab565f8885578621600b1add021b0cce1d0ee6
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2016-10-17 22:44:49 +0800

    修改依赖的库

--- work3_分词/index.js
+++ work3_分词/index.js
@@ -1,3 +1,9 @@
-var nodejieba = require("nodejieba");
-var result = nodejieba.cut("南京市长江大桥");
-console.log(result);
+// 载入模块
+var Segment = require('segment');
+// 创建实例
+var segment = new Segment();
+// 使用默认的识别模块及字典，载入字典文件需要1秒，仅初始化时执行一次即可
+segment.useDefault();
+
+// 开始分词
+console.log(segment.doSegment('这是一个基于Node.js的中文分词模块。'));
