86821143d6a7a208f376a01f017e756cbcce36a1
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2016-10-25 23:01:06 +0800

    完成分词函数

--- work3_分词/index.js
+++ work3_分词/index.js
@@ -1,52 +1,92 @@
-// // 载入模块
-// var Segment = require('segment');
-// // 创建实例
-// var segment = new Segment();
-// // 使用默认的识别模块及字典，载入字典文件需要1秒，仅初始化时执行一次即可
-// segment.useDefault();
-//
-// // 开始分词
-// console.log(segment.doSegment('这是一个基于Node.js的中文分词模块。'));
-
-
 var path=require('path'),
- iconv = require('iconv-lite'),
-  fs=require('fs');
+iconv = require('iconv-lite'),
+fs=require('fs');
 
-// fs.readFile(__dirname+'/演示语料/(国际)(1)探访日本大型太阳能电池试验场.txt',"utf-8", function(err, data) {
-//     // 读取文件失败/错误
-//     if (err) {
-//         throw err;
-//     }
-//     // 读取文件成功
-//     // var str="";
-//     console.log(data);
-//     var buf=iconv.encode(data,'gbk');
-//     console.log(buf);
-//     var str1=iconv.decode(buf,'utf-8');
-//     console.log(str1);
-//     // var buf=iconv.encode(data.toString());
-//
-//
-//     // console.log(data.toString());
-//     // console.log(data.toString());
-//
-//     // str = iconv.decode(new Buffer([0x68, 0x65, 0x6c, 0x6c, 0x6f]), 'win1251');
-//     //
-//     // buf = iconv.encode("Sample input string", 'win1251');
-//     //
-//     // iconv.encodingExists("us-ascii");
-// });
+var Dictionarys=[];
+var maxlength;
 
-var readStream=fs.createReadStream(__dirname+'/演示语料/(国际)(1)探访日本大型太阳能电池试验场.txt');
-var writeStream=fs.createWriteStream(__dirname+'/演示语料转码后/(国际)(1)探访日本大型太阳能电池试验场.txt');
+start();
+function start(){
+  getDictionary(function(data){
+    initDictionary(data,function(){
+      startparticiple();
+    });
+  });
+}
 
-readStream.on("data",function(data){
-});
-readStream.on("end",function(){
-  console.log(123);
-});
 
-readStream.pipe(iconv.decodeStream('gbk'))
-    .pipe(iconv.encodeStream('utf-8'))
-    .pipe(writeStream);
+function getDictionary(callback){
+  fs.readFile(__dirname+'/中文分词词库/30万中文分词词库.txt',"utf-8", function(err, data) {
+      if (err) {
+          throw err;
+      }
+      //console.log(data.split('\n')[1].split('\t')[0]);
+      var outdata=[];
+      data.split('\n').forEach(function(a){
+        outdata.push(a.split('\t')[1]);
+      });
+      if(typeof callback == 'function')callback(outdata);
+  });
+}
+
+function initDictionary(data,callback){
+  data.forEach(function(a){
+    if(!Dictionarys[a.length]) Dictionarys[a.length]=[];
+    Dictionarys[a.length].push(a);
+  });
+  maxlength=Dictionarys.length-1;
+  if(typeof callback == 'function')callback();
+}
+
+function startparticiple(){
+  fs.readFile(__dirname+'/演示语料转码后/(国际)(1)探访日本大型太阳能电池试验场.txt',"utf-8", function(err, data) {
+      if (err) {
+          throw err;
+      }
+      console.log(data);
+
+      // // 载入模块
+      // var Segment = require('segment');
+      // // 创建实例
+      // var segment = new Segment();
+      // // 使用默认的识别模块及字典，载入字典文件需要1秒，仅初始化时执行一次即可
+      // segment.useDefault();
+      //
+      // // 开始分词
+      // console.log(segment.doSegment(data));
+      participle(data);
+
+
+  });
+}
+
+function participle(str){
+  str=str.replace(/\s/g,"");
+  var result=[];
+  while(str){
+    var ci;
+    var l=Math.min(maxlength-1,str.length);
+    for(;l>=1;l--){
+      ci=str.substring(0,l);
+      if(l==1){
+        break;
+      }
+      if(Dictionarys[l]){
+        var ok=false;
+        Dictionarys[l].forEach(function(a){
+          if(a==ci){
+            //console.log(a);
+            ok=true;
+          }
+        });
+        if(ok)break;
+      }
+      //判断为英文或者数字
+      var ref=new RegExp(/^[\d\w\.]*$/);
+      if(ref.test(ci))break;
+    }
+    result.push(ci);
+    str=str.substring(l,str.length);
+  }
+  console.log(result);
+}
