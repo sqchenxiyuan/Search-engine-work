61471a07ce4314939d551b8db5db8659132d7d65
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2016-11-26 08:11:49 +0800

    完成加载网络

--- work5_PageRank/index.js
+++ work5_PageRank/index.js
@@ -1,10 +1,12 @@
-const fs=require('fs');
+var fs=require('fs');
+var net={};
+var num=0;
 
 fs.readFile(__dirname+'/DATA/web-BerkStan.txt',"utf-8", function(err, data) {
     if (err) {
         throw err;
     }
-    var arr=data.split('\n');
+    var arr=data.split('\r\n');
     console.log(arr.length);
     init(arr);
 });
@@ -15,5 +17,29 @@ function init(arr){
     if(edge[0] === '#'||edge[0] === ''){
       console.log(edge);
     }
+    var nodes=edge.split("\t");
+    var from=nodes[0];
+    var to=nodes[1];
+
+    if(!net[from]){
+      net[from]={
+        from:{},
+        to:{}
+      };
+      num++;
+    }
+    //
+    if(!net[to]){
+      net[to]={
+        from:{},
+        to:{}
+      };
+      num++;
+    }
+
+    net[from].to[to]=true;
+    net[to].from[from]=true;
   });
+
+  console.log(num);
 }
